26fd3cb1be4b16b6e4a2f2a793959c449ec6e4a0
cishari-cg <110623271+cishari-cg@users.noreply.github.com>
2024-12-29 03:00:31 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  return {
+    skills: [
+      { id: 1, name: 'HTML', experience: '1 year' },
+      { id: 2, name: 'CSS', experience: '1 year' },
+      { id: 3, name: 'Javascript', experience: '1 year' },
+      { id: 4, name: 'React', experience: '1 year' },
+    ]
+  }
+}
